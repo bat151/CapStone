2f0b8a993457f420de7e384156765709c9f4433a
bat151 <bat15@pct.edu>
2025-09-22 22:42:37 -0400

    First Person Camera

---
+++ CapStone/Assets/Scripts/FPC.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPC : MonoBehaviour
+{
+
+    public Transform playerBody;
+    public float mouseSens = 100f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Lock Mouse Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Mouse Input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        // Rotate camera vertically with lock so it cant flip
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
