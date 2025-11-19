807751bcb585fbacb373a03f6221446955a0da03
bat151 <bat15@pct.edu>
2025-11-19 15:32:57 -0500

    not sure

--- CapStone/Assets/Scripts/WinMenuNickname.cs
+++ CapStone/Assets/Scripts/WinMenuNickname.cs
@@ -4,20 +4,26 @@ using UnityEngine.UI;
 
 public class WinMenuNickname : MonoBehaviour
 {
+    // input field where player will enter nickname
     public TMP_InputField nicknameInput;
 
+    // when player clicks submit
     public void OnSubmit()
     {
+        // convert text to upper case
         string nickname = nicknameInput.text.ToUpper();
         if (nickname.Length != 3)
         {
+            // make sure nickname is 3 chars
             Debug.Log("Nickname must be 3 characters!");
             return;
         }
 
+        // submit the player time 
         Timer.Instance.SubmitBestTime(nickname);
         Debug.Log("Nickname submitted: " + nickname);
-        // Optionally hide the input field after submission
+
+        // hide the input field after submission
         nicknameInput.gameObject.SetActive(false);
     }
 }
